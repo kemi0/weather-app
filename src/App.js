d4f542a9de380cbd4f8155f6dda5e03fc99f0458
jeanpaul <jeanpaul.olukemi@gmail.com>
2018-04-22 13:22:46 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+class App extends React.Component {
+    render() {
+        return (
+        
+            <div>
+            <p>Welcome to react Jeanpaul</p>
+
+
+            </div>
+        );
+    }
+};
+
+export default App;
